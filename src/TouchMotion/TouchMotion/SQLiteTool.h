c0f4e4d62172a337b60376599baf044cead3a3a7
Changkun Ou <ouchangkun@outlook.com>
2016-01-02 13:59:23 +0100

    add interface explain

--- src/TouchMotion/TouchMotion/SQLiteTool.h
+++ src/TouchMotion/TouchMotion/SQLiteTool.h
@@ -17,17 +17,41 @@
 // 初始化数据库连接
 +(void)initialize;
 
-// 开启事务
+/**
+ *  开启事务
+ *
+ *  @return 是否开启成功
+ */
 +(int)beginService;
-// 提交事务
+/**
+ *  提交事务
+ *
+ *  @return 是否提交成功
+ */
 +(int)commitService;
 
-// 插入MotionData
+/**
+ *  插入MotionData
+ *
+ *  @param data MotionData数据
+ *
+ *  @return 返回是否写入成功
+ */
 + (BOOL)wirteDataWithMotionData:(MotionData *)data;
-// 插入MotionBUffer
+/**
+ *  插入MotionBuffer
+ *
+ *  @param buffer 插入MotionBuffer数据
+ *
+ *  @return 返回是否成功
+ */
 + (BOOL)writeBufferWithMotionBuffer:(MotionBuffer *)buffer;
 
-// 清空数据
+/**
+ *  清空所有的Motion数据
+ *
+ *  @return 返回是否清空成功
+ */
 + (BOOL)removeAllMotion;
 + (BOOL)removeAllMotionData;
 + (BOOL)removeAllMotionBuffer;

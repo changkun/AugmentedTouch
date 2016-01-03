b13c3f7f1bc492e0a3764dab0a3e1ce06aff2d81
Changkun Ou <ouchangkun@outlook.com>
2016-01-03 16:55:19 +0100

    add method4 all user draw

--- dataset/script/moment.py
+++ dataset/script/moment.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from loaddata import loadUserData
 from loaddata import splitMomentDataByFeature
 from loaddata import splitMomentDataByLabel
@@ -118,16 +119,17 @@ def processMethod4(userid, device, featureCondition=1, classificationCondition=1
     clfModel = classifyModel(trainingData, trainingLabel, kernel=my_kernel, max_iter=my_max_iteration)
 
     hackErrorRateTextList = []
-
+    hackErrorRateList     = []
     for testUser in xrange(1, 17):
         if testUser != userid :
             testData, testLabel = splitMomentDataByFeatureAndLabel(testUser, device, featureCondition, classificationCondition, offsetFeatureOn=offsetFeatureOn)
             trainingData, testData, trainingLabel, testLabel = train_test_split(testData, testLabel, test_size=my_test_size, random_state=my_random_state) # use same test size with method1
             error_rate = testingWithModel(testData, testLabel, clfModel)
             line = 'user ' + str(testUser) + ' hack ' + str(userid) + ', error rate: ' + str(error_rate) + '\n'
+            hackErrorRateList.append(error_rate)
             hackErrorRateTextList.append(line)
 
-    return hackErrorRateTextList
+    return hackErrorRateTextList, hackErrorRateList
 
 def processMethod1ForAllUser(featureCondition, classificationCondition, offsetFeatureOn):
     lines = []
@@ -180,7 +182,7 @@ def processMethod4ForAllUser(featureCondition, classificationCondition, offsetFe
             # if os.path.exists(filepath)==True:
             #     continue
             # else:
-            userHackRecord = processMethod4(userid, device, featureCondition=featureCondition, classificationCondition=classificationCondition, offsetFeatureOn=offsetFeatureOn)
+            userHackRecord, _ = processMethod4(userid, device, featureCondition=featureCondition, classificationCondition=classificationCondition, offsetFeatureOn=offsetFeatureOn)
             f = open(filepath, 'w')
             f.writelines(userHackRecord)
             f.close()
@@ -255,11 +257,13 @@ def Method4Threads(xyfeature):
 
     print 'Process 4 finished...'
 
-def Method1DrawThreads():
+def Method1DrawAllUser():
     for userid in xrange(1, 17):
-        drawErrorRate(userid)
+        drawErrorRate(userid, True)
     print 'Draw process finished...'
-def drawErrorRate(userid):
+
+# 画出每个用户在四个不同clf条件下，不同feature条件的error_rate
+def drawErrorRate(userid, offset):
     featureAxis = np.arange(0, 17)
 
     plt.figure(figsize=(5,13))
@@ -268,9 +272,8 @@ def drawErrorRate(userid):
         errorRateDict = { 1:[], 2:[] }
         for device in xrange(1,3):
             for featureCondition in xrange(0, 17):
-                err = processMethod1(userid, device, featureCondition=featureCondition, classificationCondition=clfCondition, offsetFeatureOn=True)
+                err = processMethod1(userid, device, featureCondition=featureCondition, classificationCondition=clfCondition, offsetFeatureOn=offset)
                 errorRateDict[device].append(err)
-
             if device==1:
                 plt.axhline(errorRateDict[device][0], color='red') # draw feature base line
                 plt.plot(featureAxis, errorRateDict[device], label='iPhone 6 Plus', color='red', linestyle='dashdot', marker='v')
@@ -293,38 +296,82 @@ def drawErrorRate(userid):
     plt.close('all')
     print 'finish save user ' + str(userid) + '.'
 
-def plotROC():
-    userid = 1
-    device = 1
-    featureCondition = 10
-    classificationCondition = 1
-    offset = True
+def Method4DrawAllUser():
+    for userid in xrange(1, 17):
+        drawMethod4AverageErrorRate(userid, False)
+    print 'Draw process finished...'
+def drawMethod4AverageErrorRate(userid, offset):
+    featureAxis = np.arange(0, 17)
 
-    np.set_printoptions(threshold='nan')
+    plt.figure(figsize=(5,13))
+    for clfCondition in xrange(1, 5):
+        plt.subplot(4, 1, clfCondition)
+        errorRateDict = { 1:[], 2:[] }
+        for device in xrange(1,3):
+            for featureCondition in xrange(0, 17):
+                _, errList = processMethod4(userid, device, featureCondition=featureCondition, classificationCondition=clfCondition, offsetFeatureOn=offset)
+                err = np.mean(errList)
+                errorRateDict[device].append(err)
+            if device==1:
+                plt.axhline(errorRateDict[device][0], color='red') # draw feature base line
+                plt.plot(featureAxis, errorRateDict[device], label='iPhone 6 Plus', color='red', linestyle='dashdot', marker='v')
+            elif device==2:
+                plt.axhline(errorRateDict[device][0], color='blue') # draw base line
+                plt.plot(featureAxis, errorRateDict[device], label='iPhone 5', color='blue', linestyle='dashdot', marker='o')
+            plt.title('Classification condition ' + str(clfCondition), fontsize='small')
+            plt.legend(loc='upper right', fontsize='small')
+            plt.axis([0, 16, 0, 1])
+            plt.ylabel('Error Rate')
+            plt.axhline(0.05, color='green') # draw error rate base line
+        print 'finish condition' + str(clfCondition)
+
+    plt.xlabel('Feature Condition')
+    plt.suptitle('User ' + str(userid))
+
+    fileName = '../result/img/result/method4/'+str(userid)+'.png'
+    #plt.show()
+    plt.savefig(fileName, dpi=72)
+    plt.close('all')
+    print 'finish save user ' + str(userid) + '.'
+
+
+def plotROC(userid, device, featureCondition, classificationCondition, offset=False, noisyOn=True):
+    # userid = 1
+    # device = 1
+    # featureCondition = 10
+    # classificationCondition = 3
+    # offset = False
+    # noisyOn = True
+    #np.set_printoptions(threshold='nan')
 
     # import data to play with
     data, label = splitMomentDataByFeatureAndLabel(userid, device, featureCondition, classificationCondition, offsetFeatureOn=offset)
 
-    print data
-    print label
-
     # binarize the output
-    label = label_binarize(label, classes=['0','1'])
-    print label
+    classes = list()
+    if classificationCondition==1:
+        classes=['0','1']
+    elif classificationCondition==2:
+        classes=['2','3']
+    elif classificationCondition==3:
+        classes=['0','1','2','3']
+    else: # classificationCondition==4
+        classes=['0','1']
+    label = label_binarize(label, classes=classes)
+
     n_classes = label.shape[1]
-    print n_classes
 
     # add noisy feature to make problem harder
-    random_state = np.random.RandomState(my_random_state)
-    n_samples, n_features = data.shape
-    print data.shape
-    data = np.c_[data, random_state.randn(n_samples, 200 * n_features)]
+    if noisyOn==True:
+        random_state = np.random.RandomState(my_random_state)
+        n_samples, n_features = data.shape
+        data = np.c_[data, random_state.randn(n_samples, 200 * n_features)]
 
     #shuffle and split traning and test sets
     trainingData, testData, trainingLabel, testLabel = train_test_split(data, label, test_size=0.5, random_state=my_random_state)
 
     # learn to predict each class against the other
-    classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True, random_state=my_random_state, max_iter=my_max_iteration))
+    classifier = OneVsRestClassifier(svm.SVC(kernel=my_kernel, probability=True, random_state=my_random_state, max_iter=my_max_iteration))
     label_score = classifier.fit(trainingData, trainingLabel).decision_function(testData)
 
     print 'decision success.'
@@ -338,78 +385,74 @@ def plotROC():
         fpr[i], tpr[i], _ =roc_curve(testLabel[:, i], label_score[:, i])
         roc_auc[i] = auc(fpr[i], tpr[i])
 
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(testLabel.ravel(), label_score.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    # # Compute micro-average ROC curve and ROC area
+    # print testLabel
+    # print label_score
+    # fpr["micro"], tpr["micro"], _ = roc_curve(testLabel.ravel(), label_score.ravel())
+    # roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
+    # # Compute macro-average ROC curve and ROC area
+    # # First aggregate all false positive rates
+    # all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    # # Then interpolate all ROC curves at this points
+    # mean_tpr = np.zeros_like(all_fpr)
+    # for i in range(n_classes):
+    #     mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    # # Finally average it and compute AUC
+    # mean_tpr /= n_classes
 
-    ##############################################################################
-    # Plot of a ROC curve for a specific class
-    # plt.figure()
-    # plt.plot(fpr[0], tpr[0], label='ROC curve (area = %0.2f)' % roc_auc[0])
-    # plt.plot([0, 1], [0, 1], 'k--')
-    # plt.xlim([0.0, 1.0])
-    # plt.ylim([0.0, 1.05])
-    # plt.xlabel('False Positive Rate')
-    # plt.ylabel('True Positive Rate')
-    # plt.title('Receiver operating characteristic')
-    # plt.legend(loc="lower right")
-    # plt.show()
-
-    ##############################################################################
-    # Plot ROC curves for the multiclass problem
-
-    # Compute macro-average ROC curve and ROC area
-
-    # First aggregate all false positive rates
-    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-
-    # Then interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
-
-    # Finally average it and compute AUC
-    mean_tpr /= n_classes
-
-    fpr["macro"] = all_fpr
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+    # fpr["macro"] = all_fpr
+    # tpr["macro"] = mean_tpr
+    # roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
 
     # Plot all ROC curves
     plt.figure()
-    plt.plot(fpr["micro"], tpr["micro"],
-             label='micro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["micro"]),
-             linewidth=2)
+    # plt.plot(fpr["micro"], tpr["micro"],
+    #          label='micro-average ROC curve (area = {0:0.2f})'
+    #                ''.format(roc_auc["micro"]),
+    #          linewidth=2)
 
-    plt.plot(fpr["macro"], tpr["macro"],
-             label='macro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["macro"]),
-             linewidth=2)
+    # plt.plot(fpr["macro"], tpr["macro"],
+    #          label='macro-average ROC curve (area = {0:0.2f})'
+    #                ''.format(roc_auc["macro"]),
+    #          linewidth=2)
 
     for i in range(n_classes):
-        plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
-                                       ''.format(i, roc_auc[i]))
+        strLabel = 'ROC curve of class {0} (area = {1:0.2f})'''.format(i, roc_auc[i])
+        plt.plot(fpr[i], tpr[i], label=strLabel)
 
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
+    plt.ylim([0.0, 1.0])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.title('Receiver operating characteristic')
     plt.legend(loc="lower right")
-    plt.show()
+
+    fileName = '../result/img/roc/clf'+str(classificationCondition)+'/featureCondition' + str(featureCondition) + '/device' + str(device) + '/user' + str(userid) + '.png'
+    plt.savefig(fileName, dpi=72)
+    print 'finish ' + fileName
+    plt.close('all')
+
+    #plt.show()
+def plotROCforAll():
+    for classificationCondition in xrange(1,5):
+        for featureCondition in xrange(0,17):
+            for device in xrange(1,3):
+                for userid in xrange(1,17):
+                    plotROC(userid, device, featureCondition, classificationCondition, offset=False, noisyOn=True)
+                    #print 'finish clf' + str(classificationCondition) + ' feature' + str(featureCondition) + ' user' + str(userid)
 
 def main():
 
-    #Method1DrawThreads()
+    #Method1DrawAllUser()
+    Method4DrawAllUser()
 
     # Method1Threads(True)
     # Method2Threads(True)
     # Method3Threads(True)
     # Method4Threads(True)
-    plotROC()
+    #plotROCforAll()
 
 
 if __name__ == '__main__':

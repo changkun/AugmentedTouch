74790d0f62de09275b02040009c67e3d621fd5fd
Changkun Ou <ouchangkun@outlook.com>
2016-01-18 08:59:27 +0100

    add something new

--- src/script/new/moment.py
+++ src/script/new/moment.py
@@ -585,6 +585,52 @@ def allTable5():
     for userid in xrange(1,17):
         forTable5(userid, clf)
 
+def feed_forward(X, weights):
+    a = [X]
+    for w in weights:
+        a.append(sigmoid(a[-1].dot(w)))
+    return a
+
+def grads(X, Y, weights):
+    grads = np.empty_like(weights)
+    a = feed_forward(X, weights)
+    delta = a[-1] - Y # cross-entropy
+    grads[-1] = np.dot(a[-2].T, delta)
+    for i in xrange(len(a)-2, 0, -1):
+        delta = np.dot(delta, weights[i].T) * d_sigmoid(a[i])
+        grads[i-1] = np.dot(a[i-1].T, delta)
+    return grads / len(X)
+
+def dnn():
+    sigmoid = lambda x: 1 / (1 + np.exp(-x))
+    d_sigmoid = lambda y: y * (1 - y)
+
+    userid=1
+    device=1
+    featureCondition=1
+    classificationCondition=1
+    offsetFeatureOn=False
+
+    data, label = splitMomentDataByFeatureAndLabel(userid, device, featureCondition, classificationCondition, offsetFeatureOn=offsetFeatureOn)
+    trX, teX, trY, teY = train_test_split(data, label, test_size=my_test_size, random_state=my_random_state)
+
+    # (trX, trY), _, (teX, teY) = mnist.load_data()
+
+
+    trY = mnist.to_one_hot(trY)
+
+    weights = [
+        np.random.randn(784, 100) / np.sqrt(784),
+        np.random.randn(100, 10) / np.sqrt(100)]
+    num_epochs, batch_size, learn_rate = 30, 10, 0.2
+
+    for i in xrange(num_epochs):
+    for j in xrange(0, len(trX), batch_size):
+        X, Y = trX[j:j+batch_size], trY[j:j+batch_size]
+        weights -= learn_rate * grads(X, Y, weights)
+    out = feed_forward(teX, weights)[-1]
+    print i, np.mean(np.argmax(out, axis=1) == teY)
+
 def main():
 
     #Method1DrawAllUser()
